4d80233e883ced16a24e73179e34fd4a676e78ed
keremozt <keremoztopuzzz@gmail.com>
2026-04-01 14:24:50 +0300

    Add Unity project files with flight controller implementation

---
+++ Assets/Scripts/FlightController.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class FlightController : MonoBehaviour
+{
+    public float speed;
+    public float maxSpeed = 500f;
+    public float minSpeed = 0f;
+    public float rotspeed1 = 25f;
+    public float rotspeed2 = 25f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        
+    }
+
+    void Update() 
+    {
+        transform.position += -transform.forward * Time.deltaTime * speed;
+
+        if(Input.GetKey(KeyCode.Space)){
+            if(speed < maxSpeed){
+                speed += 50f * Time.deltaTime;
+            }
+        }
+        if(Input.GetKey(KeyCode.LeftShift)){
+            if(speed > minSpeed){
+                speed -= 50f * Time.deltaTime;
+            }
+        }
+
+        if(Input.GetKey(KeyCode.Q)){
+            transform.Rotate(Vector3.back * rotspeed1 * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.E)){
+            transform.Rotate(Vector3.forward * rotspeed1 * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.S)){
+            transform.Rotate(Vector3.right * rotspeed2 * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.W)){
+            transform.Rotate(Vector3.left * rotspeed2 * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.D)){
+            transform.Rotate(Vector3.up * rotspeed2 * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.A)){
+            transform.Rotate(Vector3.down * rotspeed2 * Time.deltaTime);
+        }
+    }
+}
