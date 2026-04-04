08a29217aa6ca4c94b9a7ccc42a68d12a590c9cf
keremozt <keremoztopuzzz@gmail.com>
2026-04-05 00:07:23 +0300

    feat: implement missile countdown logic upon player entry and exit of danger zone

--- Assets/Scripts/DangerZoneController.cs
+++ Assets/Scripts/DangerZoneController.cs
@@ -1,8 +1,13 @@
 using UnityEngine;
+using System.Collections;
 
 public class DangerZoneController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private FlightExamController examManager;
+    [SerializeField] private MissileLauncher missileLauncher;
+    [SerializeField] private float missileDelay = 5f;
+    private Coroutine activeCountdown;
+
     void Start()
     {
         
@@ -11,6 +16,24 @@ public class DangerZoneController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+    
     }
+
+    void OnTriggerEnter(Collider other) {
+        if(!other.CompareTag("Player")) return;
+        examManager.EnterDangerZone();
+        activeCountdown = StartCoroutine(MissileCountdown());
+    }
+
+    void OnTriggerExit(Collider other) {
+        if(!other.CompareTag("Player")) return;
+        if(activeCountdown != null){
+            StopCoroutine(activeCountdown);
+            activeCountdown = null;
+            examManager.ExitDangerZone();
+        }
+
+    }
+
 }
+
