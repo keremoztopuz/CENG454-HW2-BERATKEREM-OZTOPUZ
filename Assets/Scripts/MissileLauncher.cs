c0bed134ae3181a4a51b1c7129e15e80008c44bc
keremozt <keremoztopuzzz@gmail.com>
2026-04-05 17:36:39 +0300

    feat: implement MissileLauncher script to handle missile instantiation and target homing

---
+++ Assets/Scripts/MissileLauncher.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class MissileLauncher : MonoBehaviour
+{
+    [SerializeField] private GameObject missilePrefab;
+    [SerializeField] private Transform launchPoint;
+    [SerializeField] private AudioSource launchAudio;
+
+    private GameObject activeMissile;
+
+public void Launch(Transform target){
+    GameObject spawnedMissile = Instantiate(missilePrefab, launchPoint.position, launchPoint.rotation);
+    activeMissile = spawnedMissile;
+    MissileHoming homing = activeMissile.GetComponent<MissileHoming>();
+
+    if(homing != null){
+        homing.SetTarget(target);
+        }
+
+    if (launchAudio != null) {
+        launchAudio.Play();
+        }
+    }
+
+}
+
